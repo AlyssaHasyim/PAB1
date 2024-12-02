3a17875855790bf17e3a6c07001bf5c7517d110a
Alyssa Hasyim <alyssahasyim@gmail.com>
2024-12-02 11:33:39 +0700

    kelas
    kelas

---
+++ flutter_bottom_navigation/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bottom_navigation/screens/favorite_screen.dart';
+import 'package:flutter_bottom_navigation/screens/home_screen.dart';
+import 'package:flutter_bottom_navigation/screens/main_screen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: MainScreen());
+  }
+}
